f91898ec9e053b197b154d535c49689e6eecaa41
Abhirup "Avi" Bhadore <avi.bhadore@gmail.com>
2023-02-05 10:03:18 -0500

    set up XP CSS

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,44 @@
 <template>
+  <link rel="stylesheet"  href="https://unpkg.com/xp.css">
+<div id="outmost">
   <nav>
-    <router-link to="/">Home</router-link> |
+    <div class="window" style="width: 300px">
+  <div class="title-bar">
+    <div class="title-bar-text">A Window With Stuff In It</div>
+    <div class="title-bar-controls">
+      <button aria-label="Minimize"></button>
+      <button aria-label="Maximize"></button>
+      <button aria-label="Close"></button>
+    </div>
+  </div>
+  <div class="window-body">
+    <button>
+      <router-link to="/">Home</router-link>
+    </button>
+    <button>
     <router-link to="/about">About</router-link>
+    </button>
+  </div>
+</div>
+    
   </nav>
+
+  <p> Im Mr Tweet</p>
   <router-view/>
+</div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+*{
+  margin: 0;
 }
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+#outmost{
+  background-image: url("./assets/Bliss.jpg");
+  background-repeat: no-repeat;
+  width: 100%
 }
 </style>
+
+<script>
+
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
+import AboutViewVue from '@/views/AboutView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+
 
 const routes = [
   {
@@ -13,7 +15,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: AboutViewVue
   }
 ]
 
